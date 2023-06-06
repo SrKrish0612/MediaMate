780a635cd2f19d96109ababdabe580a02d84d054
Sambhav Sharma <sambhavsharma1279@gmail.com>
2023-06-06 12:43:25 +0530

    Initial Commit

---
+++ MediaMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaMate
+//
+//  Created by Sharma, Sambhav (Contractor) on 06/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
